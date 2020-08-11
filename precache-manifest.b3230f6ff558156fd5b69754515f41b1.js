32bd541991faa304b0ddca2029bbec719c6948a0
David Mario <davidmariolc.dev@gmail.com>
2020-08-11 17:30:38 -0500

    Updates

---
+++ precache-manifest.b3230f6ff558156fd5b69754515f41b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869fb636b7f3f3e3f7f9c884f11e26b8",
+    "url": "/brakingbad-frases/index.html"
+  },
+  {
+    "revision": "46fc77b27e7392173c7b",
+    "url": "/brakingbad-frases/static/css/main.193d5b5f.chunk.css"
+  },
+  {
+    "revision": "d918c633e3c902b0988a",
+    "url": "/brakingbad-frases/static/js/2.abec3b13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brakingbad-frases/static/js/2.abec3b13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46fc77b27e7392173c7b",
+    "url": "/brakingbad-frases/static/js/main.b431dbc1.chunk.js"
+  },
+  {
+    "revision": "bdcd8b2227b8be5df960",
+    "url": "/brakingbad-frases/static/js/runtime-main.aca031dc.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/brakingbad-frases/static/media/logo.427a4adc.svg"
+  }
+]);
